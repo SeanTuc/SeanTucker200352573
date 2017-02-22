0c7f72d8fc67bc56086c0a5e4f46b4a667ba44f7
SeanTuc <sean_tucker366@hotmail.com>
2017-02-22 16:05:39 -0500

    commit one

---
+++ book_delete.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+$tableName = $_SESSION['tableID'];
+include_once('database.php');
+
+//grabs usernameId from url;
+$usernameID = $_GET["usernameID"];
+
+if($usernameID != false) {
+    // deletes row from table when usernameID is provided;
+    $query = "DELETE FROM $tableName WHERE id = $usernameID ";
+    $statement = $db->prepare($query);
+    $success = $statement->execute();
+    $statement->closeCursor();
+    echo "deleted";
+}
+
+
+
+
+/*//////////////////////*/
+/* YOUR CODE GOES HERE */
+/*/////////////////////*/
+
+// redirect to index page
+header('Location: index.php');
+
+?>
---
+++ update_database.php
@@ -0,0 +1,62 @@
+<?php
+session_start();
+$servername = 'ca-cdbr-azure-central-a.cloudapp.net';
+$dbname = 'videogamesdb';
+$userName = 'b6ee96bd470785';
+$password = 'dc381279';
+$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+// setting conn attributes on how to handle mySQL returns;
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+$isAddition = filter_input(INPUT_POST, "isAddition");
+$bookTitle = filter_input(INPUT_POST, "TitleTextField");
+$bookAuthor = filter_input(INPUT_POST, "AuthorTextField");
+$bookPrice = filter_input(INPUT_POST, "PriceTextField");
+$bookGenre = filter_input(INPUT_POST, "GenreTextField");
+
+// check if user is Adding a New Book
+if($isAddition == "1") {
+
+    $sql = "INSERT INTO $tableName ( name, Homework, feedPet, workoutTime) 
+VALUES ('$bookTitle', '$bookAuthor', '$bookPrice', '$bookGenre')";
+
+    $conn->exec($sql);
+    echo "Record created successfully";
+/*//////////////////////*/
+    /* FIX THIS MYSQL QUERY */
+/*//////////////////////*/
+
+
+}
+// else if user is Updating an Existing Book
+else {
+$bookID = filter_input(INPUT_POST, "IDTextField");
+
+
+
+/*//////////////////////*/
+/* FIX THIS MYSQL QUERY */
+/*//////////////////////*/
+    $sql = "UPDATE $tableName SET name = '$bookTitle', Homework = '$bookAuthor', feedPet = '$bookPrice', workoutTime = '$bookGenre'
+ WHERE id = $contactID "; // SQL statement
+
+    $conn->exec($sql);
+
+
+
+
+
+}
+
+/*$statement->bindValue(':book_title', $bookTitle);
+$statement->bindValue(':book_author', $bookAuthor);
+$statement->bindValue(':book_price', $bookPrice);
+$statement->bindValue(':book_genre', $bookGenre);
+$statement->execute(); // run on the db server
+$statement->closeCursor(); // close the connection
+*/
+$conn = null ;
+// redirect to index page
+header('Location: index.php');
+?>
