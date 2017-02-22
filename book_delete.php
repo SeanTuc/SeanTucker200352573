d4ab8e72847434782a5a76af585e523f1dacadc2
SeanTuc <sean_tucker366@hotmail.com>
2017-02-22 17:32:03 -0500

    o

--- book_delete.php
+++ book_delete.php
@@ -6,6 +6,7 @@ include_once('database.php');
 //grabs usernameId from url;
 $usernameID = $_GET["bookId"];
 echo $usernameID;
+
 if($usernameID != null) {
     // deletes row from table when usernameID is provided;
     $query = "DELETE FROM book WHERE title = $usernameID ";
