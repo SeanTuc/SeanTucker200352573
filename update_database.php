6fb4e654981d3e72ba0e9841bbf0275377e724f3
SeanTuc <sean_tucker366@hotmail.com>
2017-02-22 16:53:08 -0500

    work

--- update_database.php
+++ update_database.php
@@ -4,51 +4,52 @@ $servername = 'ca-cdbr-azure-central-a.cloudapp.net';
 $dbname = 'videogamesdb';
 $userName = 'b6ee96bd470785';
 $password = 'dc381279';
-$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 // setting conn attributes on how to handle mySQL returns;
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$isAddition = $_GET["isAddition"];
+    $isAddition = $_GET["isAddition"];
 
-$bookTitle = filter_input(INPUT_POST, "TitleTextField");
-$bookAuthor = filter_input(INPUT_POST, "AuthorTextField");
-$bookPrice = filter_input(INPUT_POST, "PriceTextField");
-$bookGenre = filter_input(INPUT_POST, "GenreTextField");
+    $bookTitle = filter_input(INPUT_POST, "TitleTextField");
+    $bookAuthor = filter_input(INPUT_POST, "AuthorTextField");
+    $bookPrice = filter_input(INPUT_POST, "PriceTextField");
+    $bookGenre = filter_input(INPUT_POST, "GenreTextField");
 
 // check if user is Adding a New Book
-if($isAddition == "1") {
+    if ($isAddition == "1") {
 
-    $sql = "INSERT INTO $tableName ( Title, Author, Price, Genre) 
+        $sql = "INSERT INTO $tableName ( Title, Author, Price, Genre) 
 VALUES ('$bookTitle', '$bookAuthor', '$bookPrice', '$bookGenre')";
 
-    $conn->exec($sql);
-    echo "Record created successfully";
-/*//////////////////////*/
-    /* FIX THIS MYSQL QUERY */
-/*//////////////////////*/
+        $conn->exec($sql);
+        echo "Record created successfully";
+        /*//////////////////////*/
+        /* FIX THIS MYSQL QUERY */
+        /*//////////////////////*/
 
 
-}
-// else if user is Updating an Existing Book
-else {
-$bookID = filter_input(INPUT_POST, "IDTextField");
+    } // else if user is Updating an Existing Book
+    else {
+        $bookID = filter_input(INPUT_POST, "IDTextField");
 
 
-
-/*//////////////////////*/
-/* FIX THIS MYSQL QUERY */
-/*//////////////////////*/
-    $sql = "UPDATE $tableName SET Title = '$bookTitle', Author = '$bookAuthor', Price = '$bookPrice', Genre = '$bookGenre'
+        /*//////////////////////*/
+        /* FIX THIS MYSQL QUERY */
+        /*//////////////////////*/
+        $sql = "UPDATE $tableName SET Title = '$bookTitle', Author = '$bookAuthor', Price = '$bookPrice', Genre = '$bookGenre'
  WHERE Id = $bookTitle "; // SQL statement
 
-    $conn->exec($sql);
+        $conn->exec($sql);
 
 
-
-
+    }
+}
+catch(PDOException $e)
+{
+    echo $sql . "<br>" . $e->getMessage();
 
 }
-
 /*$statement->bindValue(':book_title', $bookTitle);
 $statement->bindValue(':book_author', $bookAuthor);
 $statement->bindValue(':book_price', $bookPrice);
